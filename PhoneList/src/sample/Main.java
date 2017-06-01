d332615a4b51aaee7c6c53febbcfd427004f18a0
Moonika1991 <monika9107@gmail.com>
2017-06-01 12:35:01 +0200

    Add files via upload

---
+++ PhoneList/src/sample/Main.java
@@ -0,0 +1,41 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+    private Stage primaryStage;
+
+    @Override
+    public void start(Stage primaryStage){
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("Phone List");
+
+        showPersonOverview();
+    }
+
+    private void showPersonOverview(){
+        try{
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("view/PersonOverview.fxml"));
+            AnchorPane window = (AnchorPane) loader.load();
+
+            Scene scene = new Scene(window);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
